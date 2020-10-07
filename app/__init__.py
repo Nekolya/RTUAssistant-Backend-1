6d01d622c49c650b31aaf4602d7bb79d8f46992b
Olya <olya75825@gmail.com>
2020-10-07 17:58:09 +0300

    Big update

--- app/__init__.py
+++ app/__init__.py
@@ -21,31 +21,39 @@ class UserModel(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     fingerprint = db.Column(db.String())
-    login = db.Column(db.String())
-    password_hash = db.Column(db.String())
+    # login = db.Column(db.String())
+    secret = db.Column(db.String())
     status = db.Column(db.String())
     refresh_token = db.Column(db.String())
 
-    def __repr__(self):
-        return f"<User {self.login}>"
+    # def __repr__(self):
+    #     return f"<User {self.login}>"
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    # def set_password(self, password):
+    #     self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
 
-    def __init__(self, login, password):
-        self.login = login
-        self.password_hash = set_password(password)
-
+    # def __init__(self, login, password):
+    #     self.login = login
+    #     self.password_hash = set_password(password)
+    def refresh_allow(self, refresh_token, fingerprint):
+        if (refresh_token == self.refresh_token):
+            if fingerprint == self.fingerprint:
+                return 'ok'
+            return 'fingerprint_error'
+        return 'internal_server_error'######
+        
 
 def get_user_by_pk(pk):
     return db.session.query(UserModel).get(pk)
 
-def user_login(login, password):
-    user = s.query(UserModel).filter_by(login=login).first()
-    if user and user.check_password(password):
-        return db.session.query(UserModel).get(pk)
-    return None
+def login(secret):
+    # user = db.session.query(UserModel).filter_by(login=login).first()
+    # if user and user.check_password(password):
+    #     return db.session.query(UserModel).get(pk)
+    return db.session.query(UserModel).filter_by(secret=secret).first()
     
+
+
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, request, redirect, url_for, session, jsonify, re
 import requests
 import jwt
 from os import environ 
-
+import datetime
 
 SECRET = environ.get('SECRET')
 
@@ -11,18 +11,26 @@ SECRET = environ.get('SECRET')
 @app.route('/auth/login', methods=["POST", "GET", "OPTIONS"])
 def login():
     req = request.get_json(force=True)
-    login = req['login']
-    passw = req['password']
+    secret = req['secret']
     fingerprint = req['fingerprint']
     status = ""
-    user = user_login(login, passw)
-    if user:
+    user = login(secret)
+    
+    if user: #if secret exist
         if user.status == 'ok':
             status = 'ok'
-            encoded_jwt = jwt.encode({'user_id': user.id}, SECRET, algorithm='HS256')
             res = {"status": status}
+            access_token = jwt.encode({'user_id': user.id, 
+                                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 
+                                        SECRET, algorithm='HS256')
+
+            refresh_token = jwt.encode({'exp': datetime.datetime.utcnow() + datetime.timedelta(days=60)}, 
+                                        SECRET, algorithm='HS256')
+            
             res['access_token'] = encoded_jwt
-            res['refresh_token'] = user.refresh_token
+            res['refresh_token'] = refresh_token
+            user.update({'refresh_token': refresh_token, 'fingerprint': fingerprint})
+            db.session.commit()
 
         elif user.status == 'account_suspended':
             status = 'error'
@@ -32,49 +40,129 @@ def login():
 
     else:
         status = 'error'
-        error = 'login_incorrect'
+        error = 'secret_incorrect'
         res = {"status": status}
         res['error'] = error
 
     response = jsonify(res)
     return make_response(response)
 
-####
+#############
 @app.route('/auth/refresh_token', methods=["POST", "GET", "OPTIONS"])
 def refresh_token():
     req = request.get_json(force=True)
     refresh_token = req['refresh_token']
+    fingerprint = req['fingerprint']
     status = ""
 
-    data = jwt.decode(refresh_token, SECRET, algorithms=['HS256'])
-    user_id = data["user_id"]
-    #user = bd.get("user_id") if user exist status = ok else 
-    res = {"status": status}
-    if status == "error":
-        error = ""
-        res["error"] = error
-    
-    else:
-        access_token = ""
-        refresh_token = ""
-        res["access_token"] = access_token
-        res["refresh_token"] = refresh_token
+    try:
+        data = jwt.decode(refresh_token, SECRET, algorithms=['HS256'])
+        user_id = data["user_id"]
+        user = get_user_by_pk(user_id)
+        if user:
+            if user.status == 'ok':
+                error = user.refresh_allow(refresh_token, fingerprint)
+                if error == 'ok':
+                    status = 'ok'
+                    res = {"status": status}
+                    access_token = jwt.encode({'user_id': user.id, 
+                                                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 
+                                                SECRET, algorithm='HS256')
+
+                    refresh_token = jwt.encode({'exp': datetime.datetime.utcnow() + datetime.timedelta(days=60)}, 
+                                                SECRET, algorithm='HS256')
+                    
+                    res['access_token'] = encoded_jwt
+                    res['refresh_token'] = refresh_token
+                    user.update({'refresh_token': refresh_token})
+                    db.session.commit()
+                else:
+                    status = 'error'
+                    res = {"status": status}
+                    res['error'] = error
+            elif user.status == 'account_suspended':
+                status = 'error'
+                error = 'account_suspended'
+                res = {"status": status}
+                res['error'] = error
+        else:
+            status = 'error'
+            error = 'internal_server_error'
+            res = {"status": status}
+            res['error'] = error
+    # Signature has expired
+    except jwt.ExpiredSignatureError:
+        status = 'error'
+        error = 'token_expired'
+        res = {"status": status}
+        res['error'] = error
+    except jwt.DecodeError:
+        status = 'error'
+        error = 'internal_server_error'
+        res = {"status": status}
+        res['error'] = error
+    except jwt.InvalidSignatureError:
+        status = 'error'
+        error = 'internal_server_error'
+        res = {"status": status}
+        res['error'] = error
     response = jsonify(res)
     return make_response(response)
-
 
 ###
 @app.route('/auth/logout', methods=["POST", "GET", "OPTIONS"])
 def logout():
-    #req = request.get_json(force=True)
-    #access_token = req['access_token']
+    req = request.get_json(force=True)
+    refresh_token = req['refresh_token']
+    fingerprint = req['fingerprint']
     status = ""
-    res = {"status": status}
-    if status == "error":
-        error = ""
-        res["error"] = error
 
+    try:
+        data = jwt.decode(refresh_token, SECRET, algorithms=['HS256'])
+        user_id = data["user_id"]
+        user = get_user_by_pk(user_id)
+        if user:
+            if user.status == 'ok':
+                error = user.refresh_allow(refresh_token, fingerprint)
+                if error == 'ok':
+                    status = 'ok'
+                    res = {"status": status}
+                    refresh_token = jwt.encode({'exp': datetime.datetime.utcnow()}, 
+                                                SECRET, algorithm='HS256')
+                    
+                    user.update({'refresh_token': refresh_token})
+                    db.session.commit()
+                else:
+                    status = 'error'
+                    res = {"status": status}
+                    res['error'] = error
+            elif user.status == 'account_suspended':
+                status = 'error'
+                error = 'account_suspended'
+                res = {"status": status}
+                res['error'] = error
+        else:
+            status = 'error'
+            error = 'internal_server_error'
+            res = {"status": status}
+            res['error'] = error
+    # Signature has expired
+    except jwt.ExpiredSignatureError:
+        status = 'error'
+        error = 'token_expired'
+        res = {"status": status}
+        res['error'] = error
+    except jwt.DecodeError:
+        status = 'error'
+        error = 'internal_server_error'
+        res = {"status": status}
+        res['error'] = error
+    except jwt.InvalidSignatureError:
+        status = 'error'
+        error = 'internal_server_error'
+        res = {"status": status}
+        res['error'] = error
 
-@app.route('/')
-def mew():
-    return 'Mew'
+    data = jwt.decode(refresh_token, SECRET, algorithms=['HS256'])
+    user_id = data["user_id"]
+    user = get_user_by_pk(user_id)
